3c35d18e72c541a1fd76b61c2fc3636a99f9c8fa
Qanat <qanat92@gmail.com>
2021-10-11 18:37:23 +0500

    Добавление заявки в вакансию, геолокации на карте

--- local/components/kompot/rest.api/class.php
+++ local/components/kompot/rest.api/class.php
@@ -1,4 +1,5 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+use Bitrix\Main\Loader;
 use Bitrix\Iblock\Component\Tools as IblockTools;
 
 class CRestHelper extends CBitrixComponent
@@ -9,6 +10,11 @@ class CRestHelper extends CBitrixComponent
     const IBLOCK_BANNER_ID = 2;
     /** @var int ID инфоблока вакансий */
     const IBLOCK_VACANCY_ID = 3;
+    /** @var int ID инфоблока заявок на вакансию */
+    const IBLOCK_VACANCY_REQUEST_ID = 4;
+    /** @var int ID инфоблока геопозиций */
+    const IBLOCK_LOCATION_ID = 5;
+
 
     /**
      * Поиск метода по параметрам $entity и $method
@@ -37,6 +43,7 @@ class CRestHelper extends CBitrixComponent
      * Запуск метода
      * @param $entity
      * @param $method
+     * @throws \Bitrix\Main\LoaderException
      */
     public function runMethod ($entity, $method)
     {
@@ -54,6 +61,7 @@ class CRestHelper extends CBitrixComponent
     /**
      * Вывод 404
      * @param string $pageString
+     * @throws \Bitrix\Main\LoaderException
      */
     public function show404 ($pageString = '')
     {
@@ -194,6 +202,77 @@ class CRestHelper extends CBitrixComponent
                 "src" => CFile::GetPath($arElement['PREVIEW_PICTURE']),
                 "desc" => $arElement['PREVIEW_TEXT'],
                 "id" => $arElement['ID']
+            ];
+        }
+        $this->successAnswer($arResult);
+    }
+
+    /**
+     * Добавлении заявки на вакансию
+     */
+    public function postVacancyAdd ()
+    {
+        $vacancy_id = intval($_POST['vacancy_id']);
+        $fio = htmlspecialchars(strip_tags($_POST['fio']));
+        $birthday = htmlspecialchars(strip_tags($_POST['birthday']));
+        $gender = htmlspecialchars(strip_tags($_POST['gender']));
+        $phone = htmlspecialchars(strip_tags($_POST['phone']));
+        $email = htmlspecialchars(strip_tags($_POST['email']));
+        $resume_text = htmlspecialchars(strip_tags($_POST['resume_text']));
+        $resume_file = CFile::SaveFile(array_merge($_FILES['resume_file'], ['MODULE_ID' => "iblock"]), "iblock");
+
+        $arFields = [
+            "ACTIVE" => "Y",
+            "NAME" => "Заявка от " . date('d.m.Y H:i:s'),
+            "IBLOCK_ID" => self::IBLOCK_VACANCY_REQUEST_ID,
+            "PROPERTY_VALUES" => [
+                "VACANCY" => $vacancy_id,
+                "FIO" => $fio,
+                "BIRTHDAY" => $birthday,
+                "GENDER" => $gender,
+                "PHONE" => $phone,
+                "EMAIL" => $email,
+                "RESUME" => $resume_text,
+                "RESUME_FILE" => $resume_file,
+            ]
+        ];
+
+        $obElement = new \CIBlockElement();
+        $id = $obElement->Add($arFields);
+        if ($id > 0) {
+            $this->successAnswer(['id' => $id]);
+        } else {
+            \CFile::Delete($resume_file);
+            $this->errorAnswer("Ошибка при добавлении заявки: " . $obElement->LAST_ERROR);
+        }
+    }
+
+    /**
+     * Список геолокаций
+     */
+    public function getLocationList ()
+    {
+        $arResult = [];
+        $rsElement = CIBlockElement::GetList(['SORT' => "ASC"], [
+            "IBLOCK_ID" => self::IBLOCK_LOCATION_ID,
+            "ACTIVE" => "Y",
+            "GLOBAL_ACTIVE" => "Y"
+        ], false, false, [
+            "ID",
+            "NAME",
+            "PROPERTY_GEO",
+            "PROPERTY_TYPE",
+        ]);
+        while ($arElement = $rsElement->Fetch()) {
+            $geo = explode(",", $arElement['PROPERTY_GEO_VALUE']);
+            $fiz = $arElement['PROPERTY_GEO_VALUE_ID'] == 49;
+            $arResult[] = [
+                "pointName" => $arElement['NAME'],
+                "lat" => $geo[0],
+                "lng" => $geo[1],
+                "yur" => !$fiz,
+                "fiz" => $fiz,
+                "id" => $arElement['ID'],
             ];
         }
         $this->successAnswer($arResult);
